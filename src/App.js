26b5d6827deb27b9b333a39554cfe68298c1a47e
Matthias Wieser <matthias.wieser.83@live.de>
2022-04-26 18:04:17 +0200

    implemented Contentful Client with Test approach

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const contentful = require('contentful');
+
+    const client = contentful.createClient({
+        space: 'nar81f64yzsp',
+        environment: 'master', // defaults to 'master' if not set
+        accessToken: '5mqDvnN1n79lahTS_wM_QwQ2lxOg51XlM6L8JEpIbes'
+    })
+
+    const [entries, setEntries] = useState();
+
+    useEffect(() => {
+        client.getEntries()
+            .then((response) => {
+                console.log(response.items);
+                setEntries(response.items);
+            })
+            .catch(console.error);
+
+
+    }, []);
+
+    const test = () => {
+        console.log(entries);
+        entries.map((entry) => {
+            console.log(entry.fields);
+        });
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {entries ? test() : 'Loading...'}
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    );
 }
 
 export default App;

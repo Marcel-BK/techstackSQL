7a3afc7773474246291890b9d2d14ede84f2a950
Matthias Wieser <matthias.wieser.83@live.de>
2022-04-27 16:18:47 +0200

    created Structure to assign tasks

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+import NavContainer from './NavContainer'
+
+const NavBar = () => {
+    return (
+        <>
+            <h1>Logo</h1>
+            <h1>TechStack</h1>
+            <SearchBar />
+            <NavContainer />
+        </>
+    )
+}
+
+export default NavBar

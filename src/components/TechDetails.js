a5e90758fed006e94d60f892da5308d090b85463
Matthias Wieser <matthias.wieser.83@live.de>
2022-05-19 19:10:44 +0200

    implement related techs and author

--- src/components/TechDetails.js
+++ src/components/TechDetails.js
@@ -2,78 +2,57 @@ import { Spinner } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import RelatedTechs from './RelatedTechs';
 
 const TechDetails = (props) => {
     const { id } = useParams();
     const [tech, setTech] = useState(null);
-    // const [relatedTechs, setRelatedTechs] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios
             .get(`${process.env.REACT_APP_TECHSTACK_API}/api/techs/${id}`)
             .then(response => {
-                // const catId = response.data.tech.cat_id;
-                console.log(response.data.tech);
                 setTech(response.data.tech);
-                // axios
-                //     .get(`${process.env.REACT_APP_TECHSTACK_API}/api/techs/category/${catId}`)
-                //     .then(res2 => {
-                //         setRelatedTechs(res2.techs);
-                //         console.log(res2.techs);
-                //     })
-                //     .catch(err => console.log(err));
-                    setLoading(false);
-                })
-                .catch(err => console.log(err));
+                setLoading(false);
+            })
+            .catch(err => console.log(err));
 
-    }, []);
+    }, [id]);
 
     return (
         <>
             {
                 (loading) ? (<Spinner />) : (
-                    <div id='techDetails'>
-                        {/* <Breadcrumbs
-                    path={[tech.fields.category, id]}
-                    name={tech.fields.title}
-                /> */}
-                        <div className='bg-light'>
-                            <figure className='w-25 mx-auto py-4'>
-                                <img className='w-100' src={tech.logo_link} alt={`Logo of ${tech.title}`} />
-                            </figure>
+                    <>
+                        <div id='techDetails'>
+                            <div className='bg-light'>
+                                <figure className='w-25 mx-auto py-4'>
+                                    <img className='w-100' src={tech.logo_link} alt={`Logo of ${tech.title}`} />
+                                </figure>
+                            </div>
+                            <div>
+                                <div className='w-75 mx-auto'>
+                                    <h2>{tech.title}</h2>
+                                    <p>Author: {tech.username}</p>
+                                </div>
+                                <div
+                                    id='techDescription'
+                                    className='w-75 mx-auto'
+                                    dangerouslySetInnerHTML={{ __html: tech.description }}
+                                >
+                                </div>
+                                <div className='w-75 mx-auto wiki_link py-4'>
+                                    <a href={tech.wiki_link} target='_blank' rel="noreferrer">Go to wiki page</a>
+                                </div>
+                                <div className='w-75 mx-auto wiki_link py-4'>
+                                    <h4>Techs in same Category:</h4>
+                                    <RelatedTechs catId={tech.cat_id} />
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <div className='w-75 mx-auto'>
-                                <h2>{tech.title}</h2>
-                            </div>
-                            <div
-                                id='techDescription'
-                                className='w-75 mx-auto'
-                                dangerouslySetInnerHTML={{ __html: tech.description }}
-                            >
-                            </div>
-                            <div className='w-75 mx-auto wiki_link py-4'>
-                                <a href={tech.wiki_link} target='_blank' rel="noreferrer">Go to wiki page</a>
-                            </div>
-                        </div>q
-                    </div>
+                    </>
                 )
-            }
-            {
-                <>
-                    {/* 
-                    (loading) ? (<Spinner />) : (
-                    <div id='relatedTechs'>
-                        <h4>related Techs:</h4>
-                        {
-                            relatedTechs.map((relatedTech, index) => {
-                                return <p key={index}>{relatedTech.title}</p>
-                            })
-                        }
-                    </div>
-                    ) */}
-                </>
             }
         </>
     )
